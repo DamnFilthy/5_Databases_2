0c523e49a74f29162a8ccb8be8b455d69da9f612
Alex <alexander-shiryaev-1993@mail.ru>
2021-02-18 11:20:00 +0300

    updated views

--- m2m-relations/articles/views.py
+++ m2m-relations/articles/views.py
@@ -1,7 +1,21 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
-from articles.models import Article
+from articles.models import Article, ArticleScope
 
 
 def articles_list(request):
-    articles = Article.objects.order_by('-published_at').prefetch_related('scopes')
+    # Оптимизированная выборка сортированная по двум полям
+    articles = Article.objects.order_by('-published_at').prefetch_related(Prefetch(
+            'scopes',
+            queryset=ArticleScope.objects.select_related(
+                'topic'
+            ).order_by('-is_main', 'topic'),
+        ),
+    )
+    # Оптимизированная выборка
+    # articles = Article.objects.order_by('-published_at').prefetch_related(
+    #     Prefetch('scopes', queryset=ArticleScope.objects.order_by('topic').select_related('topic')))
+
+    # Обычная выборка
+    # articles = Article.objects.order_by('-published_at').prefetch_related('scopes')
     return render(request, 'articles/news.html', {'object_list': articles})
